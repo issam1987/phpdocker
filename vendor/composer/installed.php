264f30903a49019d77c7428709a478d2c8b31123
GHORBALI <issam.ghorbali@gmail.com>
2022-05-30 16:14:10 +0100

    action composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4ac7a151cce1de8b61999ef5b93a2eb1bac7af44',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4ac7a151cce1de8b61999ef5b93a2eb1bac7af44',
+            'dev_requirement' => false,
+        ),
+    ),
+);
